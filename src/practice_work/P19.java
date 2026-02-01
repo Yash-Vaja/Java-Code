23448404c8bb827cf8b05ea635ac54c204b29160
Yash <vajayash27092006@gmail.com>
2026-02-01 15:30:42 +0530

    Work done

--- src/practice_work/P19.java
+++ src/practice_work/P19.java
@@ -1,8 +1,41 @@
-package practice_work;/*
- Write a program to accept 5 command line argument and
-then raise the custom exception if any argument is not from
-the list(“BCA”,”MCA”,”BBA”,”MBA”,”OTHER”).
+package practice_work;
+/*
+ Write a program to accept 5 command line arguments and
+ raise a custom exception if any argument is not from
+ the list ("BCA","MCA","BBA","MBA","OTHER").
 */
 
+import java.util.Scanner;
+
 public class P19 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[5];
+
+        for (int i=0;i<5;i++){
+            System.out.print("Enter string " + (i + 1) + " : ");
+            checkCourse(sc.next());
+        }
+
+        System.out.println("All course inputs are valid");
+    }
+
+    static void checkCourse(String s) {
+        if (!(s.equalsIgnoreCase("BCA") ||
+                s.equalsIgnoreCase("MCA") ||
+                s.equalsIgnoreCase("BBA") ||
+                s.equalsIgnoreCase("MBA") ||
+                s.equalsIgnoreCase("OTHER"))) {
+
+            throw new CourseException("Invalid Course: " + s);
+        }
+    }
+}
+
+/* Custom Exception */
+class CourseException extends RuntimeException {
+    CourseException(String message) {
+        super(message);
+    }
 }
